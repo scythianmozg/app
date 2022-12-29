7a2dcf5b71fc3cc8b9bcb6c564fba14ccda85e54
scythianmozg <121483742+scythianmozg@users.noreply.github.com>
2022-12-29 22:36:00 +0300

    solved
    solved

--- Обучение 2_cycles/Ветвления/and_or_2.py
+++ Обучение 2_cycles/Ветвления/and_or_2.py
@@ -4,9 +4,7 @@ for messages_count in range(0, 21):
         print('У вас нет новых сообщений')
     elif messages_count == 1:
         print('У вас ' + str(messages_count) + ' новое сообщение')
-    elif messages_count == 2 and messages_count <= 19:
-        print('У вас ' + str(messages_count) + ' новое сообщение')
-    elif messages_count >= 2 and messages_count <= 19:
-        print('У вас ' + str(messages_count) + ' новых сообщений')
+    elif messages_count >= 2 and messages_count <= 4:
+        print('У вас ' + str(messages_count) + ' новых сообщения')
     else:
-       print('У вас ' + str(messages_count) + ' новых сообщений')
+        print('У вас ' + str(messages_count) + ' новых сообщений')

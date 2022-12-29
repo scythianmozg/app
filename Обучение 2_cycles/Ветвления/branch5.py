97330dbf99a6802fec33e49fce43082496972c60
scythianmozg <121483742+scythianmozg@users.noreply.github.com>
2022-12-29 22:18:10 +0300

    bags
    bags

--- Обучение 2_cycles/Ветвления/branch5.py
+++ Обучение 2_cycles/Ветвления/branch5.py
@@ -1,3 +1,3 @@
-for messages_count in range(0, 6):
-    if messages_count > 0:
-    print('Новых сообщений: ' + messages_count)
+# for messages_count in range(0, 6):
+# if messages_count > 0
+# print('Новых сообщений: ' + messages_count)

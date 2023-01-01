f3e370f8b21bdd5edb151574a46a727ecbd99883
scythianmozg <121483742+scythianmozg@users.noreply.github.com>
2023-01-01 18:48:45 +0300

    Create counter3.py

---
+++ Обучение 2/Counter/counter3.py
@@ -0,0 +1,15 @@
+flat = [
+    5.55, 22.19, 7.78, 26.86, 5.55, 29.84, 22.19, 5.55, 16.85, 4.52, 7.78]
+
+# len(flat) подсчитает количество элементов в списке flat
+# Сохраним это значение в переменную rooms_num
+rooms_num = len(flat)
+room_size = 7.78
+room_count = 0
+for room in flat:
+    if room == room_size:
+        room_count = room_count + 1
+
+# И напечатаем полученное значение
+print('Всего комнат ' + rooms_num)
+print('Всего комнат размером 7.78 ' + room_count)

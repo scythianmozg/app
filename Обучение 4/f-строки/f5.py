a579ee2ac0f1b096a77adbec9d28dc64a725c707
scythianmozg <121483742+scythianmozg@users.noreply.github.com>
2023-01-10 16:57:18 +0300

    f
    f

---
+++ Обучение 4/f-строки/f5.py
@@ -0,0 +1,5 @@
+def calc_stat(listened):  # От англ. calculate statistics, посчитать статистику
+    count = len(listened)
+    return f'Вы прослушали {count} песен.'
+
+print(calc_stat([189, 148, 210, 144, 174, 158, 163, 189, 227, 198]))

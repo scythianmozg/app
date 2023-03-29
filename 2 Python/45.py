88c2a35bc1b4a266746c9a08e6ac56b971f93a3f
scythianmozg <121483742+scythianmozg@users.noreply.github.com>
2023-03-29 16:57:57 +0300

    ok
    ok

--- 2 Python/45.py
+++ 2 Python/45.py
@@ -5,19 +5,19 @@ lengh1 = int(len(name1))
 lengh2 = int(len(name2))
 lengh3 = int(len(name3))
 
-len_max =  max(lengh1, lengh2, lengh3)
+len_max = max(lengh1, lengh2, lengh3)
 len_min = min(lengh1, lengh2, lengh3)
 
-if lengh_min == lengh1:
+if len_min == lengh1:
     print(name1)
-elif lengh_min == lengh2:
+elif len_min == lengh2:
     print(name2)
 else:
     print(name3)
 
-if lengh_max == lengh1:
+if len_max == lengh1:
     print(name1)
-elif lengh_max == lengh2:
+elif len_max == lengh2:
     print(name2)
 else:
     print(name3)

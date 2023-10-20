3a9216516c1ada95a4b8c554252c304a76e22d0e
scythianmozg <naziz.kz@gmail.com>
2023-10-20 13:48:42 +0600

    Create df 7.py

---
+++ Practicum 1/df 7.py
@@ -0,0 +1,9 @@
+import pandas as pd
+df = pd.read_csv('music_log.csv')
+
+# сохраняем Series в отдельную переменную
+total_play = df['total play']
+
+# через логическую индексацию получаем недослушанные треки,
+# и считаем их методом count()
+print(total_play[total_play <= 10].count()) 
